dfe020c3369c15654e356c74e32e56728c8dd48b
manish785 <kumarachilish1997@gmail.com>
2023-05-30 19:49:54 +0530

    Connected to the Database

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 const express = require('express');
 const { route } = require('express/lib/application');
 const port = 8080;
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
 const app = express();
+
+app.use(express.static('./assets'))
+app.use(expressLayouts);
+
+dotenv.config();
+mongoose.connect(process.env.MONGO_URL)
+.then(()=>console.log("DB connection Succesful")).catch((err)=>{
+    console.log(err);
+});
+
+
+// extract style and scripts from sub pages into the layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
 
 
 // use express router
 app.use('/', require('./routes'));
 
-
-
+// set up the view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
 
 
