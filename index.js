c365fa07b8be16610cc7d68883d4d211e06a7dde
manish785 <kumarachilish1997@gmail.com>
2023-06-24 21:00:02 +0530

    added social-authentication, can login using gmail account as well

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const dotenv = require('dotenv');
 const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
+const passportJWT = require('./config/passport-jwt-strategy');
 const MongoStore = require('connect-mongo');
 const sassMiddleware = require('node-sass-middleware');
 const flash = require('connect-flash');
